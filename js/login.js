5164010bfee6ff6d68380f9203f5306b60d074cc
April <yeon19april@gmail.com>
2021-10-31 12:23:48 +0900

    Waffle design

--- js/login.js
+++ js/login.js
@@ -1,16 +1,20 @@
-const todoContainer = document.querySelector("#todo_container");
+// const todoContainer = document.querySelector("#todo_container");
+const todoContainer = document.querySelector("#logged-in-container");
 const login = document.querySelector("#login_container");
 const registerContainer = document.querySelector("#register");
 const loginId = document.querySelector("#login_id");
 const loginPw = document.querySelector("#login_pw");
 const loginBtn = document.querySelector("#login_btn");
-const logoutBtn = document.querySelector("#logout_btn");
+// const logoutBtn = document.querySelector("#logout_btn");
+const logoutBtn = document.querySelector("#logout-text");
 const registerBtn = document.querySelector("#register_btn");
 const registerId = document.querySelector("#register_id");
 const registerPw = document.querySelector("#register_pw");
 const confirmBtn = document.querySelector("#confirm_btn");
 const cancelBtn = document.querySelector("#cancel_btn");
 const greetingMsg = document.querySelector("#greeting");
+const weatherPanel = document.querySelector("#weather");
+const clockPanel = document.querySelector("#clock");
 
 function checkValue(){
     const ID = loginId.value;
@@ -59,8 +63,10 @@ function logIn(){
     login.classList.add("hidden")
     registerContainer.classList.add("hidden")
     todoContainer.classList.remove("hidden")
+    weatherPanel.classList.add("logged-in-clock-weather")
+    clockPanel.classList.add("logged-in-clock-weather")
 
-    greetingMsg.innerText = `Hello ${localStorage.getItem("loggedIn")}`
+    greetingMsg.innerText = `Hello ${localStorage.getItem("loggedIn")}!`
 
 }
 
@@ -68,6 +74,8 @@ function logOut(){
     login.classList.remove("hidden")
     registerContainer.classList.add("hidden")
     todoContainer.classList.add("hidden")
+    weatherPanel.classList.remove("logged-in-clock-weather")
+    clockPanel.classList.remove("logged-in-clock-weather")
     localStorage.removeItem("loggedIn");
 }
 
